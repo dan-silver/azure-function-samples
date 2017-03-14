d48a0d5cd50fbbca6b85b4245afed538cd0cc4c4
Dan Silver <dansil@microsoft.com>
2017-03-13 17:44:15 -0700

    1

--- check-weather/function.js
+++ check-weather/function.js
@@ -1,33 +1,38 @@
-// import { OpenWeatherMapAppId } from '../secrets'
-// import fetch from 'node-fetch';
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const secrets_1 = require("../secrets");
+const node_fetch_1 = require("node-fetch");
 function main(context, req) {
-    console.log('a1');
-    context.done(null, { body: { a: 1 } });
-    // return;
-    // getWeatherForecast(req.query.city).then((weatherData) => {
-    //     console.log('a2')
-    //     let response = {
-    //         status: 200, // optional, defaults to 200
-    //         body: weatherData
-    //     };
-    //     context.done(null, response);
-    // }).catch((error) => {
-    //     console.log('a3')
-    //     // try passing error as first param
-    //     context.done(null, {
-    //         status: 500,
-    //         body: {error1: error}
-    //     });
-    // })
+    getWeatherForecast(req.query.city).then((weatherData) => {
+        let response = {
+            status: 200,
+            body: weatherData
+        };
+        context.done(null, response);
+    }).catch((error) => {
+        // try passing error as first param
+        context.done(null, {
+            status: 500,
+            body: { error1: error }
+        });
+    });
 }
 exports.main = main;
 ;
-// async function getWeatherForecast(cityName:string):Promise<any> {
-//     if (typeof cityName === "undefined") {
-//         return Promise.reject("city not passed");
-//     }
-//     let rawRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&mode=json&APPID=${OpenWeatherMapAppId}`);
-//     let weatherObj =  await rawRes.json();
-//     return weatherObj.list[0];
-// } 
+function getWeatherForecast(cityName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (typeof cityName === "undefined") {
+            return Promise.reject("city not passed");
+        }
+        let rawRes = yield node_fetch_1.default(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&mode=json&APPID=${secrets_1.OpenWeatherMapAppId}`);
+        let weatherObj = yield rawRes.json();
+        return weatherObj.list[0];
+    });
+}
--- check-weather/function.ts
+++ check-weather/function.ts
@@ -1,36 +1,31 @@
-// import { OpenWeatherMapAppId } from '../secrets'
-// import fetch from 'node-fetch';
+import { OpenWeatherMapAppId } from '../secrets'
+import fetch from 'node-fetch';
 
 export function main (context, req) {
-    console.log('a1')
 
-    context.done(null, {body:{a:1}});
-// return;
-    // getWeatherForecast(req.query.city).then((weatherData) => {
-    //     console.log('a2')
-    //     let response = {
-    //         status: 200, // optional, defaults to 200
-    //         body: weatherData
-    //     };
-    //     context.done(null, response);
-    // }).catch((error) => {
-    //     console.log('a3')
+    getWeatherForecast(req.query.city).then((weatherData) => {
+        let response = {
+            status: 200, // optional, defaults to 200
+            body: weatherData
+        };
+        context.done(null, response);
+    }).catch((error) => {
 
-    //     // try passing error as first param
+        // try passing error as first param
 
-    //     context.done(null, {
-    //         status: 500,
-    //         body: {error1: error}
-    //     });
-    // })
+        context.done(null, {
+            status: 500,
+            body: {error1: error}
+        });
+    })
 };
 
-// async function getWeatherForecast(cityName:string):Promise<any> {
-//     if (typeof cityName === "undefined") {
-//         return Promise.reject("city not passed");
-//     }
+async function getWeatherForecast(cityName:string):Promise<any> {
+    if (typeof cityName === "undefined") {
+        return Promise.reject("city not passed");
+    }
 
-//     let rawRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&mode=json&APPID=${OpenWeatherMapAppId}`);
-//     let weatherObj =  await rawRes.json();
-//     return weatherObj.list[0];
-// }
+    let rawRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&mode=json&APPID=${OpenWeatherMapAppId}`);
+    let weatherObj =  await rawRes.json();
+    return weatherObj.list[0];
+}
